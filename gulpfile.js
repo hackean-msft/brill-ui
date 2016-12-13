167348dee7eb8a30b4f712dddb0a5473036a5dbf
hackeanwarley <akinwaletimileyin@gmail.com>
2016-12-13 21:45:05 +0100

    Configureation for ui router

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
 var gulp = require("gulp")
+var browserSync = require("browser-sync").create()
 
-gulp.task("hello", function() {
-	console.log("Hello")
+gulp.task("serve", ["copy-html", "copy-assets", "copy-app"], function() {
+	browserSync.init({
+		server: "./dist"
+	});
+
+	gulp.watch("./index.html", ["copy-html", "copy-app", "copy-assets"]);
+	gulp.watch("*.html").on("change", browserSync.reload);
+});
+
+gulp.task("copy-html", function() {
+	gulp.src("./index.html")
+		.pipe(gulp.dest("./dist"));
+});
+
+gulp.task("copy-assets", function(){
+	gulp.src("./assets/**")
+		.pipe(gulp.dest("./dist/assets"))
+})
+
+gulp.task("copy-app", function(){
+	gulp.src("./app/**")
+		.pipe(gulp.dest("./dist/app"))
 })
